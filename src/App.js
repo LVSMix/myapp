84fb11ec51f3e16d5688a8ca673274999c3276ea
LVSMix <lvsmix@gmail.com>
2023-04-06 23:12:04 -0300

    se agrega portal

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import './styles/App.css';
 import { useNavigate} from "react-router-dom";
 import Dashboard from "./pages/dashboard";
+import Modal  from "./components/modal";
 
 
 
@@ -10,13 +11,30 @@ function withRouter(Component) {
 }
 
 class App extends Component {
- 
+  
+  constructor(props){
+     super(props);
+     this.state = {modoVisible: false}
+  }
 
+  modalClickHandler = (e) => {
+    this.setState({modalVisible : false});
+  }
+  
+  confirm = () =>{
+    this.setState({modalVisible : true});
+  }
 
   render(){
       return (
         <div className="App">
           <Dashboard />
+          <button onClick={this.confirm}>Click me!!</button>
+          <div onClick={this.modalClickHandler}>
+            <Modal>
+             {this.state.modalVisible && "Hola mundo"}
+            </Modal>
+          </div>
         </div>
       );
   }
